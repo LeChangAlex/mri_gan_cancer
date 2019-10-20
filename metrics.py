b9b9a3ec87f25862ba9b41bc94a38317bcb3e91b
moturuab <le.chang@mail.utoronto.ca>
2019-10-19 22:23:16 -0400

    fixed FID

--- metrics.py
+++ metrics.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy as sp
 from MRIDataset import *
 import torch.nn as nn
+from scipy import linalg
 
 class DomainFD:
     def __init__(self, ae, ae_resolution, device):
@@ -30,7 +31,7 @@ class DomainFD:
         self.real_cov = np.cov(latent, rowvar=False)
 
 
-    def calculate_fd(self, gen_data):
+    def calculate_fd(self, gen_data, eps=1e-6):
         # gen_gmm = sklearn.mixture.GaussianMixture(n_components=self.n_components)
         # gen_gmm.fit(gen_data)
         """
@@ -46,9 +47,45 @@ class DomainFD:
         gen_mean = np.mean(latent, axis=0)
         gen_cov = np.cov(latent, rowvar=False)
 
-        diff = self.real_mean - gen_mean
-        a = self.real_cov.dot(gen_cov, out=gen_cov)
+        # diff = self.real_mean - gen_mean
+        # a = self.real_cov.dot(gen_cov)
+        # b = np.trace(self.real_cov + gen_cov - 2 * (a ** 0.5))
+        # fd = diff.dot(diff) + np.trace(self.real_cov + gen_cov - 2 * (a ** 0.5))
 
-        fd = diff.dot(diff) + np.trace(self.real_cov + gen_cov - 2 * (a ** 0.5))
+        # --------
+        mu1 = self.real_mean
+        mu2 = gen_mean
 
+        sigma1 = self.real_cov
+        sigma2 = gen_cov
+
+        print(mu1, mu2)
+        print(sigma1, sigma2)
+
+        diff = mu1 - mu2
+
+        # Product might be almost singular
+        # covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+        covmean = sigma1.dot(sigma2) ** 0.5
+
+        if not np.isfinite(covmean).all():
+            msg = ('fid calculation produces singular product; '
+                   'adding %s to diagonal of cov estimates') % eps
+            print(msg)
+            offset = np.eye(sigma1.shape[0]) * eps
+            covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+        # Numerical error might give slight imaginary component
+        if np.iscomplexobj(covmean):
+            # if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            #     m = np.max(np.abs(covmean.imag))
+            #     raise ValueError('Imaginary component {}'.format(m))
+            covmean = covmean.real
+
+        tr_covmean = np.trace(covmean)
+
+        fd = (diff.dot(diff) + np.trace(sigma1) +
+                np.trace(sigma2) - 2 * tr_covmean)
+        print(fd)
         return fd
+
