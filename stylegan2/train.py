c561f335f66c63ecb10c29841b31fb952ec4ff8a
Alex Chang <le.chang@mail.utoronto.ca>
2020-01-25 19:35:33 -0500

    wandb image logging

--- stylegan2/train.py
+++ stylegan2/train.py
@@ -258,6 +258,17 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
             )
 
             if wandb and args.wandb:
+                with torch.no_grad():
+                    g_ema.eval()
+                    sample, _ = g_ema([sample_z])
+                    print(sample.shape)
+                    # utils.save_image(
+                    #     sample,
+                    #     f'sample/{str(i).zfill(6)}.png',
+                    #     nrow=int(args.n_sample ** 0.5),
+                    #     normalize=True,
+                    #     range=(-1, 1),
+                    # )
                 wandb.log(
                     {
                         'Generator': g_loss_val,
@@ -268,20 +279,26 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
                         'Real Score': real_score_val,
                         'Fake Score': fake_score_val,
                         'Path Length': path_length_val,
+
+                        "G(z)":[wandb.Image(sample[i][0], mode="F") for i in range(sample.shape[0])]
                     }
                 )
 
-            if i % 100 == 0:
-                with torch.no_grad():
-                    g_ema.eval()
-                    sample, _ = g_ema([sample_z])
-                    utils.save_image(
-                        sample,
-                        f'sample/{str(i).zfill(6)}.png',
-                        nrow=int(args.n_sample ** 0.5),
-                        normalize=True,
-                        range=(-1, 1),
-                    )
+
+
+
+            # if i % 100 == 0:
+            #     with torch.no_grad():
+            #         g_ema.eval()
+            #         sample, _ = g_ema([sample_z])
+
+            #         utils.save_image(
+            #             sample,
+            #             f'sample/{str(i).zfill(6)}.png',
+            #             nrow=int(args.n_sample ** 0.5),
+            #             normalize=True,
+            #             range=(-1, 1),
+            #         )
 
             if i % 10000 == 0:
                 torch.save(
